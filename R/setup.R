92024616f07ca77180da8d9b3a7a17ed1ed3f14f
Anoosh Poorian <anooshpoorian@Daniels-MacBook-Pro-2.local>
2020-04-07 16:25:22 -0400

    test

---
+++ R/setup.R
@@ -0,0 +1,13 @@
+#setup
+
+options(repos='http://cran.rstudio.com/')
+
+
+#install.packages("reticulate")
+#install.packages("osmdata")
+#install.packages("dodgr")
+#install.packages("devtools")
+#install_github("anooshpoorian/MSSI/MSSI")
+#additional
+
+
