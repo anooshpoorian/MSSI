4fc0a1a69018c77d6f6b235206acf3472dbb5b19
Anoosh Poorian <anoosh.poorian@mail.mcgill.ca>
2020-04-08 14:01:33 -0400

    added README

--- R/setup.R
+++ R/setup.R
@@ -7,7 +7,13 @@ options(repos='http://cran.rstudio.com/')
 #install.packages("osmdata")
 #install.packages("dodgr")
 #install.packages("devtools")
-#install_github("anooshpoorian/MSSI/MSSI")
-#additional
+#library(devtools)
+#install_github("anooshpoorian/MSSI")
+#install.packages("rmarkdown")
+#install.packages("cancensus")
+#install.packages("spocc")
+#install.packages("knitr")
+#install.packages("tinytex")
+#use_readme_rmd(open = rlang::is_interactive())
 
 
