b535b5f9a88496d4470378a9c0039c59b4a7638f
Anoosh Poorian <anoosh.poorian@mail.mcgill.ca>
2020-04-09 19:05:59 -0400

    renamed datalist upload to datalist update

---
+++ R/datalist_update.R
@@ -0,0 +1,30 @@
+library(cancensus)
+library(git2rdata)
+library(git2r)
+
+upload_cancensus_list <- function(){
+  system("git init")
+  repo <- repository(".git/")
+  system("git remote rm origin")
+  system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
+  system("git pull origin")
+  data <- cancensus::list_census_datasets()
+  pull(repo)
+  write_vc(data, file = "datasets", root = repo, stage = TRUE)
+  commit(repo, "datalist updated")
+  system("git push -u origin master")
+}
+
+update_datalist <- function(name, description){
+  system("git init")
+  repo <- repository(".git/")
+  system("git remote rm origin")
+  system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
+  system("git pull origin")
+  olddata <- read_vc(file = "datasets", root = repo)
+  newdf<-rbind(olddata, data.frame(dataset=name, description=description))
+  write_vc(newdf, file = "datasets", root = repo, stage = TRUE)
+  commit(repo, "datalist updated")
+  system("git push -u origin master")
+}
+
