ce9ab834f4ddcbdea1d3b86dea99cd7ce8b994fc
Anoosh Poorian <anoosh.poorian@mail.mcgill.ca>
2020-04-09 12:47:04 -0400

    Dataset scripts added

---
+++ R/datalist.R
@@ -0,0 +1,7 @@
+library(git2rdata)
+library(git2r)
+
+#system("git remote rm origin")
+#system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
+#system("git pull -u origin master")
+read.table(file = 'datasets.tsv', sep = '\t', header = TRUE)
---
+++ R/datalist_upload.R
@@ -0,0 +1,14 @@
+library(cancensus)
+library(git2rdata)
+library(git2r)
+
+
+system("git init")
+repo <- repository(".git/")
+data <- cancensus::list_census_datasets()
+pull(repo)
+write_vc(data, file = "datasets", root = repo, stage = TRUE)
+commit(repo, "data added")
+system("git remote rm origin")
+system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
+system("git push -u origin master")
