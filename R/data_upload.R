5f44e30a1ca20abf22635016997745d287e5dc8f
Anoosh Poorian <anoosh.poorian@mail.mcgill.ca>
2020-04-09 21:12:11 -0400

    debugging with data upload

--- R/data_upload.R
+++ R/data_upload.R
@@ -3,7 +3,7 @@ data_upload <- function(df, name){
   repo <- repository(".git/")
   system("git remote rm origin")
   system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
-  pull(repo)
+  #pull(repo)
   write_vc(df, file = name, t = repo, stage = TRUE)
   commit(repo, "datalist updated")
   system("git push -u origin master")
