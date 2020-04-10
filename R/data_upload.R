146f655d2d9ce283efa691e4c298e0618a67cc5e
Anoosh Poorian <anoosh.poorian@mail.mcgill.ca>
2020-04-09 22:37:18 -0400

    added data upload

--- R/data_upload.R
+++ R/data_upload.R
@@ -1,4 +1,4 @@
-data_upload <- function(df, name){
+data_upload_dataframe <- function(df, name){
   system("git init")
   repo <- repository(".git/")
   system("git remote rm origin")
@@ -6,5 +6,14 @@ data_upload <- function(df, name){
   #pull(repo)
   write_vc(df, file = name, t = repo, stage = TRUE)
   commit(repo, "datalist updated")
+  system("git push -u origin master")
+}
+
+data_upload <- function(obj, name){
+  system("git init")
+  system("git remote rm origin")
+  system("git remote add origin https://github.com/anooshpoorian/SUSDeveloper.git")
+  saveRDS(obj, name + ".rds")
+  commit(repo, "data uploaded")
   system("git push -u origin master")
 }
